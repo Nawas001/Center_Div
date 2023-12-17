b65a1e3a36c9edaec03bda49278c112f534eb278
nawasudeen.m <nawasudeen.m@kgisl.com>
2023-12-17 12:25:42 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './center-div/layout/layout/layout.component';
+import { HomePageComponent } from './center-div/screens/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  //{ path: "**", redirectTo: "home", pathMatch: "full" },
+
+  //center div routing
+  { path: "home", component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './center-div/layout/layout/layout.component';
+import { NavBarComponent } from './center-div/layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './center-div/layout/footer/footer.component';
+import { HomePageComponent } from './center-div/screens/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomePageComponent
+   
   ],
   imports: [
     BrowserModule,
